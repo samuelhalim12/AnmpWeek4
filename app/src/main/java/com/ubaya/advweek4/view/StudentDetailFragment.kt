a40a27861a9b54aa1db27de164bb0fcaa79047b3
samuelhalim12 <samuelhalim12@gmail.com>
2022-03-03 02:17:50 +0700

    Finished

--- app/src/main/java/com/ubaya/advweek4/view/StudentDetailFragment.kt
+++ app/src/main/java/com/ubaya/advweek4/view/StudentDetailFragment.kt
@@ -5,14 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ubaya.advweek4.R
+import com.ubaya.advweek4.model.Student
+import com.ubaya.advweek4.viewmodel.DetailViewModel
+import com.ubaya.advweek4.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.fragment_student_detail.*
+import kotlinx.android.synthetic.main.fragment_student_list.*
 
 class StudentDetailFragment : Fragment() {
+    private lateinit var  viewModel: DetailViewModel
+    private val studentListAdapter = StudentListAdapter(arrayListOf())
+    private lateinit var arrStudent : Student
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_student_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        viewModel.fetch()
+
+        observeViewModel()
+    }
+    private fun observeViewModel() {
+        viewModel.studentLiveData.observe(viewLifecycleOwner) {
+//            studentListAdapter.updateStudentList(it)
+            arrStudent = Student(
+                viewModel.studentLiveData.value?.id,viewModel.studentLiveData.value?.name,
+                viewModel.studentLiveData.value?.dob,viewModel.studentLiveData.value?.phone,
+            viewModel.studentLiveData.value?.photoURL)
+            editID.setText(arrStudent.id)
+            editName.setText(arrStudent.name)
+            editDOB.setText(arrStudent.dob)
+            editPhone.setText(arrStudent.phone)
+        }
     }
 }
--- app/src/main/java/com/ubaya/advweek4/view/StudentListFragment.kt
+++ app/src/main/java/com/ubaya/advweek4/view/StudentListFragment.kt
@@ -5,15 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ubaya.advweek4.R
+import com.ubaya.advweek4.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.fragment_student_list.*
 
 class StudentListFragment : Fragment() {
-
+    private lateinit var  viewModel: ListViewModel
+    private val studentListAdapter = StudentListAdapter(arrayListOf())
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_student_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        recView.layoutManager = LinearLayoutManager(context)
+        recView.adapter = studentListAdapter
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.studentsLiveData.observe(viewLifecycleOwner) {
+            studentListAdapter.updateStudentList(it)
+        }
+        viewModel.studentsLoadErrorLiveData.observe(viewLifecycleOwner) {
+            textError.visibility = if (it) View.VISIBLE else View.GONE
+        }
+        viewModel.loadingLiveData.observe(viewLifecycleOwner) {
+            if (it) { //sedang loading
+                recView.visibility = View.GONE
+                progressLoad.visibility = View.VISIBLE
+            } else {
+                recView.visibility = View.VISIBLE
+                progressLoad.visibility = View.GONE
+            }
+        }
     }
 }
